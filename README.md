5b067b59e6969dcb4fda91fde4f44367a6c19312
Kaj Jagtenberg <info@kajjagtenberg.nl>
2021-08-14 10:54:07 +0000

    Updated docs

--- README.md
+++ README.md
@@ -15,12 +15,26 @@ go get github.com/kajjagtenberg/pack
 ```Go
 namespace := []byte("users")
 user := []byte("johndoe123")
+version := pack.Uint64ToBytes(uint64(100))
 
 key := pack.Pack(
     namespace,
     user,
-) // dXNlcnM=:am9obmRvZTEyMw==
+    version
+)
 
 // Set a key in your favorite key-value store
 db.Set(key, value)
+```
+
+## API
+
+```Go
+func Pack(values ...[]byte) []byte
+
+func Unpack(value []byte) [][]byte
+
+func Uint64ToBytes(value uint64) []byte
+
+func BytesToUint64(value []byte) uint64
 ```
--- pack.go
+++ pack.go
@@ -10,6 +10,8 @@ var (
 	SEPERATOR = []byte(":")
 )
 
+// Encodes the values to base32 to be able to safely seperated them by the SEPERATOR, which is a colon.
+// It is not optimal use of the available bytes, but often key safety is more important than the size.
 func Pack(values ...[]byte) []byte {
 	for i := 0; i < len(values); i++ {
 		b := make([]byte, base64.StdEncoding.EncodedLen(len(values[i])))
@@ -20,6 +22,7 @@ func Pack(values ...[]byte) []byte {
 	return bytes.Join(values, SEPERATOR)
 }
 
+// Returns the decoded values from a packed byte array. Simply the inverse of the Pack() function.
 func Unpack(value []byte) [][]byte {
 	values := bytes.Split(value, SEPERATOR)
 
@@ -32,12 +35,14 @@ func Unpack(value []byte) [][]byte {
 	return values
 }
 
+// Quick way to convert an uint64 to bytes for packing.
 func Uint64ToBytes(value uint64) []byte {
 	result := make([]byte, 8)
 	binary.BigEndian.PutUint64(result, value)
 	return result
 }
 
+// Quick way to convert bytes to an uint64 after unpacking.
 func BytesToUint64(value []byte) uint64 {
 	return binary.BigEndian.Uint64(value)
 }
